d6c15c02ccc1c835363d8ab53a29f76d136c8fd1
Jayasri <jayasriagadi@gmail.com>
2023-05-31 23:48:15 +0530

    Uploading the project

---
+++ userproject/home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+#password for username 'neha123' is $$$$$$$$$$
+# Create your views here.
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,'index.html')
+
+def loginUser(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+        # check if user has entered correct credentials
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+           
+            return redirect("/")
+        else:
+            # No backend authenticated the credentials
+            return render(request,'login.html')
+
+    return render(request,'login.html')
+def logoutUser(request):
+    
+    logout(request)
+    return redirect("/login")
